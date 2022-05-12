7e838ab6920dba02174c3e21589bad630e68cdcd
pieer <pieer44@gmail.com>
2022-05-12 21:02:49 +0200

    Replaced null values in train data and test data with the median value.

--- PreProcessing/Preprocessing.py
+++ PreProcessing/Preprocessing.py
@@ -1,9 +1,13 @@
+from statistics import median
+
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sn
 from sklearn.preprocessing import OneHotEncoder
 from sklearn import preprocessing
+
 pd.set_option('display.max_columns', None)
+
 
 def preprocessing():
     # Date Reading
@@ -34,7 +38,6 @@ def preprocessing():
     test_data['Spending_Score'] = test_data['Spending_Score'].replace(['Average'], 1)
     test_data['Spending_Score'] = test_data['Spending_Score'].replace(['High'], 2)
 
-
     OneHotEncoder_prof = pd.get_dummies(train_data['Profession'], prefix='Profession')
     train_data = train_data.join(OneHotEncoder_prof)
     train_data.drop(['Profession'], axis=1, inplace=True)
@@ -45,19 +48,24 @@ def preprocessing():
 
     OneHotEncoder_var = pd.get_dummies(train_data['Var_1'], prefix='Var_1')
     train_data = train_data.join(OneHotEncoder_var)
-    train_data.drop([ 'Var_1'], axis=1, inplace=True)
+    train_data.drop(['Var_1'], axis=1, inplace=True)
 
     OneHotEncoder_var = pd.get_dummies(test_data['Var_1'], prefix='Var_1')
     test_data = test_data.join(OneHotEncoder_var)
     test_data.drop(['Var_1'], axis=1, inplace=True)
 
-    print(test_data.head())
-    train_data = train_data.dropna()
-    test_data = test_data.dropna()
+    # print(train_data.isnull().sum(axis=0))
+    # REPLACE NULLS WITH THE MEDIAN VALUE OF THE COLUMN.
+    train_data['Ever_Married'] = train_data['Ever_Married'].fillna(median(train_data['Ever_Married']))
+    train_data['Graduated'] = train_data['Graduated'].fillna(median(train_data['Graduated']))
+    train_data['Work_Experience'] = train_data['Work_Experience'].fillna(median(train_data['Work_Experience']))
+    train_data['Family_Size'] = train_data['Family_Size'].fillna(median(train_data['Family_Size']))
+    test_data['Ever_Married'] = test_data['Ever_Married'].fillna(median(test_data['Ever_Married']))
+    test_data['Graduated'] = test_data['Graduated'].fillna(median(test_data['Graduated']))
+    test_data['Work_Experience'] = test_data['Work_Experience'].fillna(median(test_data['Work_Experience']))
+    test_data['Family_Size'] = test_data['Family_Size'].fillna(median(test_data['Family_Size']))
 
-
-    #print(test_data.isnull().sum(axis=0))
     return train_data, test_data
 
-#preprocessing()
 
+preprocessing()
