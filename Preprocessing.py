6950ac3f66b99a3501d4c750093e1740ff74a447
Sara <saranor@gmail.com>
2022-05-12 12:44:42 +0200

    Preprcessing with 2 hotEncoding and drop nulls

--- Preprocessing.py
+++ Preprocessing.py
@@ -2,30 +2,62 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sn
 from sklearn.preprocessing import OneHotEncoder
-
+from sklearn import preprocessing
+pd.set_option('display.max_columns', None)
 
 def preprocessing():
     # Date Reading
-    train_data = pd.read_csv('../data/train.csv')
-    test_data = pd.read_csv('../data/test.csv')
+    train_data = pd.read_csv('data/train.csv')
+    test_data = pd.read_csv('data/test.csv')
 
     # 1-Filter input features
     # print(data['Holiday'].unique()) #['No Holiday' 'Holiday']
     train_data['Gender'] = train_data['Gender'].replace(['Male'], 1)
     train_data['Gender'] = train_data['Gender'].replace(['Female'], 0)
+    test_data['Gender'] = test_data['Gender'].replace(['Male'], 1)
+    test_data['Gender'] = test_data['Gender'].replace(['Female'], 0)
 
     train_data['Ever_Married'] = train_data['Ever_Married'].replace(['Yes'], 1)
     train_data['Ever_Married'] = train_data['Ever_Married'].replace(['No'], 0)
+    test_data['Ever_Married'] = test_data['Ever_Married'].replace(['Yes'], 1)
+    test_data['Ever_Married'] = test_data['Ever_Married'].replace(['No'], 0)
 
     train_data['Graduated'] = train_data['Graduated'].replace(['Yes'], 1)
     train_data['Graduated'] = train_data['Graduated'].replace(['No'], 0)
+    test_data['Graduated'] = test_data['Graduated'].replace(['Yes'], 1)
+    test_data['Graduated'] = test_data['Graduated'].replace(['No'], 0)
 
-    one_hot_encoder = OneHotEncoder(sparse=False, handle_unknown='error', drop='first')
-    OneHotEncoder_prof = pd.DataFrame(one_hot_encoder.fit_transform(train_data[['Profession']]))
-    train_data.drop(['Profession'], axis=1,inplace=True)
-    train_data=train_data.join(OneHotEncoder_prof)
+    train_data['Spending_Score'] = train_data['Spending_Score'].replace(['Low'], 0)
+    train_data['Spending_Score'] = train_data['Spending_Score'].replace(['Average'], 1)
+    train_data['Spending_Score'] = train_data['Spending_Score'].replace(['High'], 2)
+    test_data['Spending_Score'] = test_data['Spending_Score'].replace(['Low'], 0)
+    test_data['Spending_Score'] = test_data['Spending_Score'].replace(['Average'], 1)
+    test_data['Spending_Score'] = test_data['Spending_Score'].replace(['High'], 2)
 
-    print(train_data.head())
 
-    print(train_data.isnull().sum(axis=0))
+    OneHotEncoder_prof = pd.get_dummies(train_data['Profession'], prefix='Profession')
+    train_data = train_data.join(OneHotEncoder_prof)
+    train_data.drop(['Profession'], axis=1, inplace=True)
+
+    OneHotEncoder_prof = pd.get_dummies(test_data['Profession'], prefix='Profession')
+    test_data = test_data.join(OneHotEncoder_prof)
+    test_data.drop(['Profession'], axis=1, inplace=True)
+
+    OneHotEncoder_var = pd.get_dummies(train_data['Var_1'], prefix='Var_1')
+    train_data = train_data.join(OneHotEncoder_var)
+    train_data.drop([ 'Var_1'], axis=1, inplace=True)
+
+    OneHotEncoder_var = pd.get_dummies(test_data['Var_1'], prefix='Var_1')
+    test_data = test_data.join(OneHotEncoder_var)
+    test_data.drop(['Var_1'], axis=1, inplace=True)
+
+    print(test_data.head())
+    train_data = train_data.dropna()
+    test_data = test_data.dropna()
+
+
+    #print(test_data.isnull().sum(axis=0))
     return train_data, test_data
+
+#preprocessing()
+
